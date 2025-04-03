083a001a782d168049f541a366bcbc9cb1a2ce80
Teju0320 <tejasripadmarao@gmail.com>
2025-04-03 15:49:07 +0530

    BG COLOR OF TEXT

--- demo.js
+++ demo.js
@@ -1,14 +1,5 @@
-//HTML STYLES 
-const header = document.getElementById('header');
-header.style.backgroundColor = 'green';
-header.style.borderBottom = '3px solid orange';
-
-const mainheading = document.getElementById('main-heading');
-mainheading.textContent = 'Fruit World';
-mainheading.style.color = 'orange';
-
-const basketheading = document.getElementById('basket-heading');
-basketheading.style.color = 'green';
-
-const thanks = document.getElementById('thanks');
-thanks.innerHTML = '<p>Please visit us again</p>'
+const fruit = document.getElementsByClassName('fruit');
+fruit[2].style.backgroundColor = 'yellow';
+for(let i = 0; i < fruit.length; i++){
+    fruit[i].style.fontWeight = 'bold';
+}
