b5c862a14438fabc74b77df3a312617ae43d197a
Teju0320 <tejasripadmarao@gmail.com>
2025-04-03 18:34:36 +0530

    querySelector and querySelectorAll

--- demo.js
+++ demo.js
@@ -1,6 +1,34 @@
-const listItems = document.getElementsByTagName('li');
+const mainheading = document.querySelector('#main-heading');
+mainheading.style.textAlign = 'Right';
 
-for (let i = 0; i < listItems.length; i++){
-    listItems[i].style.fontStyle = 'italic';
+const basketheading = document.querySelector('#basket-heading');
+basketheading.style.color = 'brown';
+basketheading.style.margin = '30px';
+
+const fruits = document.querySelector('.fruits');
+fruits.style.backgroundColor = 'grey';
+fruits.style.padding = '30px';
+fruits.style.borderRadius = '5px';
+fruits.style.margin = '30px';
+fruits.style.listStyleType = 'none';
+
+const fruitItems = document.querySelectorAll('.fruit');
+for (let i = 0; i < fruitItems.length; i++){
+    fruitItems[i].style.padding = '10px';
+    fruitItems[i].style.margin = '10px'
+    fruitItems[i].style.borderRadius = '5px';
 }
-listItems[4].style.color = 'blue';
+
+const OddFruitItems = document.querySelectorAll('.fruit:nth-child(odd)');
+for (let i = 0; i< OddFruitItems.length; i++){
+    OddFruitItems[i].style.backgroundColor = 'white';
+   
+}
+
+const EvenFruitItems = document.querySelectorAll('.fruit:nth-child(even)');
+for (let i = 0; i < EvenFruitItems.length; i++) {
+    EvenFruitItems[i].style.backgroundColor = 'brown';
+    EvenFruitItems[i].style.color = 'white';
+}
+
+
